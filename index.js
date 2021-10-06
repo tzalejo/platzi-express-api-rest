6c1ef4d879b825ea95b40750dc58d9c2de936956
tzalejo <tzalejo@gmail.com>
2021-10-06 00:24:35 -0300

    iniciando el servidor express

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('iniciando');
+const express = require('express');
+const app = express();
+const port = 3005;
+// def ruta
+app.get('/', (req, res)=>{
+    res.send('hola iniciando server express');
+});
+
+app.listen(port, ()=>{
+    console.log('mi port', port);
+});
+
+
+
+
