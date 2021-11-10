45d5985e9c7d0cccfd2b48e69efd721e8416cc69
tzalejo <tzalejo@gmail.com>
2021-11-10 00:03:27 -0300

    agrego metodo delete

--- routes/products.js
+++ routes/products.js
@@ -39,5 +39,24 @@ router.post('/', (req, res)=>{
     });
 });
 
+router.put('/:id', (req, res)=>{
+    const {id} = req.params;
+    const body = req.body;
+    res.json({
+        message: 'modificar',
+        data: body,
+        id,
+    });
+});
+
+
+router.delete('/:id', (req, res)=>{
+    const {id} = req.params;
+    res.json({
+        message: 'eliminar',
+        id,
+    });
+});
+
 module.exports = router;
 
