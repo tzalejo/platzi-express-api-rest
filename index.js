882747e4adcc230bfc60714c42152190e538d70f
alejandro <tzalejo@gmail.com>
2021-11-15 11:20:58 -0300

    agrego cors

--- index.js
+++ index.js
@@ -5,11 +5,28 @@ const {
   logErrors,
   boomErrorHandler,
 } = require('./middleware/error.handler');
+const cors = require('cors');
 const app = express();
 const port = 3005;
 
+// cors
+// const whitelist = // son los mismos (si pego los links platzi me impide comentar)
+// const options = {
+//   origin: (origin, callback) => {
+//     if (whitelist.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('no permitido'), false);
+//     }
+//   }
+// }
+
+// app.use(cors(options));
+app.use(cors());
+
 //respuesta en json
 app.use(express.json());
+
 // routas
 routerApi(app);
 
